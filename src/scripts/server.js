32b23256dd6975c8388b7252239725fe69455b11
Martin08ABM <martin08abm@gmail.com>
2025-05-01 14:52:18 +0200

    Añadiendo pago con Stripe

---
+++ src/scripts/server.js
@@ -0,0 +1,26 @@
+// This is your test secret API key.
+const stripe = require('stripe')('sk_test_51RJtAFGaEq7QBuHdOVCsGSgj8LjVkC8o9wBwQJckZWB3R7BWX4HTy3HfbPVB74InB3pRMqzlIVH0SXBzOWLpYaQZ00xC6I2ymD');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const YOUR_DOMAIN = 'http://localhost:4321';
+
+app.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+        line_items: [
+        {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price: '{{PRICE_ID}}',
+            quantity: 1,
+        },
+        ],
+        mode: 'payment',
+        success_url: `${YOUR_DOMAIN}/success.html`,
+        cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+    });
+
+    res.redirect(303, session.url);
+});
+
+app.listen(4321, () => console.log('Running on port 4321'));
