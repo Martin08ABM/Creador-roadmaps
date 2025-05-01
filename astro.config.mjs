eac989e70ab221ea968b94d65dee4de34f039306
Martin08ABM <martin08abm@gmail.com>
2025-05-01 22:29:32 +0200

    Arreglateeee yaaaaa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
-import clerk from "@clerk/astro";
-
-import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from '@tailwindcss/vite';
+import clerk from '@clerk/astro';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +13,28 @@ export default defineConfig({
     output: 'server',
     vite: {
         plugins: [tailwindcss()],
-    }
+        ssr: {
+        // Externaliza módulos built-in de Node para silenciar advertencias de Vite
+        external: [
+            'fs',
+            'path',
+            'url',
+            'crypto',
+            'http',
+            'https',
+            'stream',
+            'buffer',
+            'zlib',
+            'child_process',
+            'os',
+            'util',
+            'module',
+            'net',
+            'dns',
+            'assert',
+            'events',
+            'tty',
+        ],
+        },
+    },
 });
